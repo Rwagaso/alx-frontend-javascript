d83c281afc2878df6db12c975390526458644c81
Rwagaso <stevenrwa31@gmail.com>
2024-06-24 07:23:13 -0700

    more

--- 0x00-ES6_basic/3-default-parameter.js
+++ 0x00-ES6_basic/3-default-parameter.js
@@ -1,8 +1,4 @@
-
-rt default function getSumOfHoods(
-  initialNumber,
-  expansion1989 = 89,
-  expansion2019 = 19
-) {
+export default function getSumOfHoods(initialNumber, expansion1989 = 89, expansion2019 = 19) {
+  /* eslint-disable */
   return initialNumber + expansion1989 + expansion2019;
-}
+} 
