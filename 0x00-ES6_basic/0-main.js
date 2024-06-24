5607b079d2c1878166a52f905074497a0e8a0a92
Rwagaso <stevenrwa31@gmail.com>
2024-06-24 06:31:34 -0700

    0-constants.js

---
+++ 0x00-ES6_basic/0-main.js
@@ -0,0 +1,8 @@
+mport { taskFirst, taskNext } from './0-constants.js';
+
+console.log(`${taskFirst()} ${taskNext()}`);
+
+bob@dylan:~$ 
+bob@dylan:~$ npm run dev 0-main.js 
+I prefer const when I can. But sometimes let is okay
+bob@dylan:~$ 
