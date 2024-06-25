1a89ad818c367a10a77fded22593b520f56cd192
Rwagaso <stevenrwa31@gmail.com>
2024-06-25 12:38:49 -0700

    more

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -1,10 +1,16 @@
-mport { uploadPhoto, createUser } from './utils';
+import { createUser, uploadPhoto } from './utils';
 
 export default function handleProfileSignup() {
+  // Call the uploadPhoto function to upload the user's photo then chain all
+  // promises together and handle with one error statement
   return Promise
     .all([uploadPhoto(), createUser()])
-    .then((res) => {
-      console.log(`${res[0].body} ${res[1].firstName} ${res[1].lastName}`);
+    .then((userResp) => {
+      // If the user was successfully created, log the body, firstName, and lastName to the console
+      console.log(`${userResp[0].body} ${userResp[1].firstName} ${userResp[1].lastName}`);
     })
-    .catch(() => console.log('Signup system offline'));
+    .catch(() => {
+      // In the event of an error, log Signup system offline to the console
+      console.log('Signup system offline');
+    });
 }
