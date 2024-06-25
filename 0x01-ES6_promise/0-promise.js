3ba7d3495d3c160d6e8db10b0c43167352e672dc
Rwagaso <stevenrwa31@gmail.com>
2024-06-25 12:06:09 -0700

    more

---
+++ 0x01-ES6_promise/0-promise.js
@@ -0,0 +1,11 @@
+
+rt default function getRespondFromAPI() {
+  /* eslint-disable */
+  return new Promise((resolve, reject) => {
+      if (true) {
+	resolve();
+      } else {
+	reject();
+      }
+  });
+}
